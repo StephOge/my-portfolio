f01ddda15b562e7d083efd7caaa3f3bf712751a6
stephoge <nwachukwustephenie0@gmail.com>
2023-08-04 18:39:24 -0700

    portfolio-app

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
       <form method='POST' action="https://getform.io/f/8f548ecd-4d81-4de8-9efb-3551d459bbd0" className='flex flex-col max-w-[600px] w-full'>
         <div className='pb-8'>
           <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-          <p className='text-gray-300 py-4'>Submit the form below  or send a mail - nwachukwustepehenie0@gmail.com</p>
+          <p className='text-gray-300 py-4'>Submit the form below  or send a mail - "nwachukwustepehenie0@gmail.com"</p>
         </div>
         <input className='p-2 bg-[#ccd6f6]' type="text" placeholder="Name" name="name"/>
         <input className='my-4 p-2 bg-[#ccd6f6]' type="email" placeholder="Email" name="email"/>
